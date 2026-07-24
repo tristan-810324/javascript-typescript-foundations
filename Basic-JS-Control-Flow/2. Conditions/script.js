12bce197eb8bd2f564c7295e2b9db6b46d4b1852
Tristan Ivan Bautista <tristanivanbautista81@gmail.com>
2026-07-24 14:22:12 +0800

    training

--- Basic-JS-Control-Flow/2. Conditions/script.js
+++ Basic-JS-Control-Flow/2. Conditions/script.js
@@ -66,11 +66,10 @@ if (temperature > 30) {
 If the username is equal to:  Hello Tristan!
 */  
 
-const userName = "tristan"; 
+const username = "Tristan";
 
-if (userName === "tristan") { 
-        console.log("hello {UserName}") ; 
-
+if (username === "Tristan") {
+    console.log("Hello Tristan!");
 }
 
 
@@ -109,4 +108,130 @@ if (customerMoney >= productPrice) {
 
 
 
+// HARD CHALLENGES
+const productPrice = 48999;
+const customerMoney = 60000;
+const productStock = 5;
 
+if (customerMoney >= productPrice) {
+    console.log("Customer has enough money.");
+}
+
+if (productStock > 0) {
+    console.log("Product is available.");
+}
+
+
+// 2NND HARD CHALLENGES 
+ 
+const movieAgeRestriction = 18; 
+const customerAge = 21; 
+const availableSeats = 30; 
+
+
+if (movieAgeRestriction >= customerAge) { 
+        console.log("Age Verified"); 
+} 
+
+if (availableSeats > 0 ) { 
+        console.log("Seat Available");
+}
+
+
+/* 
+May ATM machine.
+let balance = 15000;
+let withdraw = 5000;
+Kapag sapat ang balance, ipakita: Withdrawal Approved
+*/
+
+let balance = 15000; 
+let withdraw = 5000; 
+
+if (balance > withdraw) { 
+    console.log("withdrawal approved"); 
+}
+
+
+
+
+
+/* Pwede lang manood ng horror movie kapag 18 pataas.
+let age = 21;
+Kapag qualified: Access Granted
+*/
+
+let age = 21; 
+if (age > 18) { 
+    console.log("Aceess Granted"); 
+}
+
+
+
+/* Kapag ang total purchase ay 5000 o higit pa, may libreng shipping.
+let total = 6800;
+output: Free Shipping
+*/
+
+let total = 6800; 
+
+if (total >= 5000) { 
+    console.log("this is now a free shipping"); 
+}
+
+/* A university only displays the names of students who qualify 
+for the Academic Excellence Scholarship.
+A student qualifies if their GPA is 1.50 or better.
+let studentName = "Tristan";
+let gpa = 1.25;
+Expected Output
+Congratulations Tristan!
+You qualified for the Academic Excellence Scholarship.
+*/ 
+
+
+let studentName = "Tristan"; 
+let gpa = 1.25; 
+
+
+if (gpa <= 1.50) { 
+    console.log(`congrats ${studentName} you are qualified for academic shcolar`); 
+}
+
+
+
+/*  
+Employees who have worked for the company for 15 years or 
+more are included in the Hall of Fame.
+
+let employee = "Carlos";
+let yearsOfService = 18;
+Expected Output
+Carlos has been added to the Hall of Fame.  */
+
+
+let employee = "Carlos"; 
+let yearsOfService = 18; 
+
+if (yearsOfService >= 15) { 
+    console.log(`${employee} has been added to the hall of fame`); 
+}
+
+
+/* 
+University Dean's List
+A university prints the names of students who earned a GPA of exactly 1.00.
+let student = "Tristan";
+let gpa = 1.00;
+Expected Output
+Highest Honors Awarded to Tristan.
+
+*/
+
+
+let student = "Tristan";
+let gpa = 1.00;
+
+if (gpa === 1.00) {
+    console.log(`Highest Honors Awarded to ${student}.`);
+}
